6dc329b383416fb9271c87d77c7f087439c14936
Julien Finet <julien.finet@kitware.com>
2012-11-18 15:22:58 -0500

    Build Libs in Bender project instead of Armatures module
    Issue #13632

---
+++ CMake/BenderExportLibrary.cmake
@@ -0,0 +1,40 @@
+#============================================================================
+#
+# Program: Bender
+#
+# Copyright (c) Kitware Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#============================================================================
+
+macro(bender_export_library)
+  set(target ${PROJECT_NAME})
+  set_property(GLOBAL APPEND PROPERTY Bender_INCLUDE_DIRS
+    ${CMAKE_CURRENT_SOURCE_DIR}
+    ${CMAKE_CURRENT_BINARY_DIR}
+    )
+  if (TARGET ${target})
+    set_property(GLOBAL APPEND PROPERTY Bender_LIBRARIES
+      ${target}
+      )
+    if(WIN32)
+      get_target_property(output_directory ${target} RUNTIME_OUTPUT_DIRECTORY)
+    else()
+      get_target_property(output_directory ${target} LIBRARY_OUTPUT_DIRECTORY)
+    endif()
+    set_property(GLOBAL APPEND PROPERTY Bender_LIBRARY_DIRS
+      ${output_directory}
+      )
+  endif()
+endmacro()
