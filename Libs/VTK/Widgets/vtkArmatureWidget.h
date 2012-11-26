03492cbd02430e29877bf5d4eef7f4949571bed4
Johan Andruejol <johan.andruejol@kitware.com>
2012-11-26 10:49:13 -0500

    Rename vtkArmatureWidget signals
    See Issue #13733

--- Libs/VTK/Widgets/vtkArmatureWidget.h
+++ Libs/VTK/Widgets/vtkArmatureWidget.h
@@ -231,9 +231,9 @@ public:
   //BTX
   enum ArmatureEventType
     {
-    AddedBone = vtkCommand::UserEvent+1,
-    RemovedBone,
-    ReparentedBone
+    BoneAdded = vtkCommand::UserEvent+1,
+    BoneRemoved,
+    BoneReparented,
     };
   //ETX
 
