28a04c32a8637a6d6a53737a61256f63aafffd9a
Johan Andruejol <johan.andruejol@kitware.com>
2012-11-28 13:25:35 -0500

    Register BonesRepresentation
    The BonesRepresentation should be registered by the armature using it.
    
    See Issue #13733

--- Libs/VTK/Widgets/vtkArmatureWidget.cxx
+++ Libs/VTK/Widgets/vtkArmatureWidget.cxx
@@ -475,7 +475,12 @@ void vtkArmatureWidget::SetBonesRepresentation(vtkBoneRepresentation* newRep)
     return;
     }
 
+  if (this->BonesRepresentation)
+    {
+    this->BonesRepresentation->Delete();
+    }
   this->BonesRepresentation = newRep;
+  this->BonesRepresentation->Register(this);
   for (NodeIteratorType it = this->Bones->begin();
     it != this->Bones->end(); ++it)
     {
