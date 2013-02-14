KitwareMedical/Bender

commit 4aa830bb6be9f997a0ddbc4e96896a79cc23be25

Add support for localized transforms in PoseBody
When a surface is partially rigged, the points outside the rig resulted of
a 0 division (wSum = 0).

Modules/CLI/PoseBody/PoseBody.cxx
@@ -895,34 +895,37 @@ int main( int argc, char * argv[] )
       wSum+=w_pi[i];
       }
 
-    Vec3 y(0.0);
-    if(LinearBlend)
+    assert(wSum>=0);
+    Vec3 y(xraw);
+    if (wSum > 0.0)
       {
-      assert(wSum>=0);
-      for(int i=0; i<numSites;++i)
+      if(LinearBlend)
         {
-        double w = w_pi[i]/wSum;
-        const RigidTransform& Fi(transforms[i]);
-        double yi[3];
-        Fi.Apply(xraw,yi);
-        y+= w*Vec3(yi);
+        for(int i=0; i<numSites;++i)
+          {
+          double w = w_pi[i]/wSum;
+          const RigidTransform& Fi(transforms[i]);
+          double yi[3];
+          Fi.Apply(xraw,yi);
+          y+= w*Vec3(yi);
+          }
         }
-      }
-    else
-      {
-      Mat24 dq;
-      dq.Fill(0.0);
-      for(int i=0; i<numSites;++i)
+      else
         {
-        double w = w_pi[i]/wSum;
-        Mat24& dq_i(dqs[i]);
-        dq+= dq_i*w;
+        Mat24 dq;
+        dq.Fill(0.0);
+        for(int i=0; i<numSites;++i)
+          {
+          double w = w_pi[i]/wSum;
+          Mat24& dq_i(dqs[i]);
+          dq+= dq_i*w;
+          }
+        Vec4 q;
+        Vec3 t;
+        DQ2QuatTrans((const double (*)[4])&dq(0,0), &q[0], &t[0]);
+        y = Vec3(xraw);
+        ApplyQT(q,t,&y[0]);
         }
-      Vec4 q;
-      Vec3 t;
-      DQ2QuatTrans((const double (*)[4])&dq(0,0), &q[0], &t[0]);
-      y = Vec3(xraw);
-      ApplyQT(q,t,&y[0]);
       }
 
     outPoints->SetPoint(pi,y[0],y[1],y[2]);

commit 1ba573ca25cff646a56f8e16f64725036cc79b6d

Fix linear blend regression
Regression introduced by 4aa830bb6be9
Issue #13919

Modules/CLI/PoseBody/PoseBody.cxx
@@ -904,8 +904,12 @@ int main( int argc, char * argv[] )
       }
 
     assert(wSum>=0);
-    Vec3 y(xraw);
-    if (wSum > 0.0)
+    Vec3 y(0.0);
+    if (wSum <= 0.0)
+      {
+      y = xraw;
+      }
+    else
       {
       if(LinearBlend)
         {
@@ -931,7 +935,7 @@ int main( int argc, char * argv[] )
         Vec4 q;
         Vec3 t;
         DQ2QuatTrans((const double (*)[4])&dq(0,0), &q[0], &t[0]);
-        y = Vec3(xraw);
+        y = xraw;
         ApplyQT(q,t,&y[0]);
         }
       }