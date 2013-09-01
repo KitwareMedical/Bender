968bdc609c085803bbcb80dcd1b73f8070aabb3c
Julien Finet <julien.finet@kitware.com>
2013-09-01 09:58:08 -0400

    Fix Models module crash when loading armature with string array

--- CMake/Superbuild/External_Slicer.cmake
+++ CMake/Superbuild/External_Slicer.cmake
@@ -79,7 +79,7 @@ if(NOT DEFINED ${proj}_DIR)
     BINARY_DIR ${${proj}_DIR}
     PREFIX ${proj}${ep_suffix}
     GIT_REPOSITORY "git://public.kitware.com/Bender/Slicer.git"
-    GIT_TAG "67493c6bf11e3b9c5b57bba48da3314e8f644815"
+    GIT_TAG "dabe11290061a89345e870df0dee6e3fd606d873"
     ${bender_external_update}
     INSTALL_COMMAND ""
     CMAKE_GENERATOR ${gen}
